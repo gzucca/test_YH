321c310c4e5a4aa24050ddb2a033a41138cb5868
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-06-29 14:43:29 +0200

    Fetch to binance API and commission env variable

--- src/services/getPrices.js
+++ src/services/getPrices.js
@@ -1,13 +1,19 @@
 module.exports = async () => {
   try {
-    const data = {
-      symbol: "LTCBTC",
-      bidPrice: "4.00000000",
-      bidQty: "431.00000000",
-      askPrice: "4.00000200",
-      askQty: "9.00000000",
+    const response = await fetch(
+      "https://api.binance.com/api/v3/ticker/bookTicker?symbol=BNBBTC"
+    );
+    const data = await response.json();
+    const { symbol, bidPrice, askPrice } = data;
+    const bidWCommission = bidPrice * (1 + Number(process.env.COMMISSION));
+    const askWCommission = askPrice * (1 + Number(process.env.COMMISSION));
+    const returnedValues = {
+      symbol,
+      bidWCommission,
+      askWCommission,
     };
-    return data;
+    console.log(data, returnedValues);
+    return returnedValues;
   } catch (error) {
     console.log("Could not get prices from API:", error.message);
   }
